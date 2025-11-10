fc29a92af4bb1059b163e44d2760710f47dba496
FilipChyla <96245628+FilipChyla@users.noreply.github.com>
2025-11-10 22:11:50 +0100

    Initialize Spring Boot project with Gradle configuration, PostgreSQL Docker setup, and essential project files.

---
+++ src/main/java/edu/chylaozgaoldakowski/location_manager/ShopLocationManagerApplication.java
@@ -0,0 +1,13 @@
+package edu.chylaozgaoldakowski.location_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopLocationManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopLocationManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/chylaozgaoldakowski/location_manager/ShopLocationManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.chylaozgaoldakowski.location_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopLocationManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
