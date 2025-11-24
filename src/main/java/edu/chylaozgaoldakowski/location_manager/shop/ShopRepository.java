6fc1b09823d0d1fb245d2ada634ce8db194b62e8
FilipChyla <96245628+FilipChyla@users.noreply.github.com>
2025-11-24 09:11:41 +0100

    Revert "Implement shop management functionality with CRUD operations and UI integration"

--- src/main/java/edu/chylaozgaoldakowski/location_manager/shop/ShopRepository.java
+++
@@ -1,6 +0,0 @@
-package edu.chylaozgaoldakowski.location_manager.shop;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShopRepository extends JpaRepository<Shop, Long> {
-}
